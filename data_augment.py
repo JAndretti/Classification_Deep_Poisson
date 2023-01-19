d90eaf8bbc310b3eb3dac175fc7dda71e4f376c0
JAndretti <jules.andretti@edu.esiee.fr>
2023-01-19 15:14:41 +0100

    unification script + retrain + new classes

--- data_augment.py
+++ data_augment.py
@@ -1,29 +1,33 @@
 import Augmentor
 import os
-import shutil
 
+# Affiche la version d'Augmentor en cours d'utilisation
 print(Augmentor.__version__)
 
-# Créez un objet "pipeline" avec votre jeu de données d'images
-folder_paths = ['data/ange', 'data/clown','data/goldfish','data/lion','data/scalaire'] 
+def augment(folder_paths):
 
-for folder in folder_paths:
-    p = Augmentor.Pipeline(folder,output_directory = '')
+    # Pour chaque dossier de la liste folder_paths
+    for folder in folder_paths:
+        # Créez un objet "pipeline" en utilisant la fonction Augmentor.Pipeline() 
+        # en spécifiant le chemin du dossier et l'emplacement de sortie pour les images générées.
+        p = Augmentor.Pipeline(folder,output_directory = '')
 
-    # Ajoutez des opérations de transformation au pipeline
-    p.rotate(probability=0.7, max_left_rotation=10, max_right_rotation=10)
-    p.zoom(probability=0.5, min_factor=1.1, max_factor=1.5)
-        # Miroir horizontal
-    p.flip_left_right(probability=0.5)
+        # Ajoutez des opérations de transformation au pipeline
+        # Rotation avec une probabilité de 0.7 et une rotation maximale de 10 degrés à gauche et 10 degrés à droite
+        p.rotate(probability=0.7, max_left_rotation=10, max_right_rotation=10)
+        # Zoom avec une probabilité de 0.5 et un facteur de zoom compris entre 1.1 et 1.5
+        p.zoom(probability=0.5, min_factor=1.1, max_factor=1.5)
+        # Miroir horizontal avec une probabilité de 0.5
+        p.flip_left_right(probability=0.5)
 
-    # Miroir vertical
-    p.flip_top_bottom(probability=0.5)
+        # Miroir vertical avec une probabilité de 0.5
+        p.flip_top_bottom(probability=0.5)
 
-    # Changement de couleur
-    p.random_color(probability=0.5, min_factor=0.8, max_factor=1.2)
+        # Changement de couleur avec une probabilité de 0.5 et un facteur de couleur compris entre 0.8 et 1.2
+        p.random_color(probability=0.5, min_factor=0.8, max_factor=1.2)
 
-    # Générez de nouvelles données en exécutant le pipeline
-    sample = 1500-len(os.listdir(folder))
-    if sample > 0:
-        p.sample(sample)
-
+        # Générez de nouvelles données en exécutant le pipeline
+        # sample est le nombre d'images générées en soustraire le nombre d'images existantes dans le dossier de l'original.
+        sample = 1500-len(os.listdir(folder))
+        if sample > 0:
+            p.sample(sample)
--- script_redimensionne.py
+++ script_redimensionne.py
@@ -4,48 +4,45 @@ import cv2
 
 
 
+def redim(poisson):
 
-poisson = ['ange', 'clown','goldfish','lion','scalaire']
-try: 
+
     os.mkdir('new_data')
-    for poisson in poisson:
-        os.mkdir('new_data/'+poisson)
-except OSError:
-    pass  # Le dossier existe déjà, on ignore l'exception
+    for fish in poisson:
+        os.mkdir('new_data/'+fish)
 
+    #Définir le nouveau chemin pour stocker les images redimensionnées
+    new_path = 'new_data/'
+    print(poisson)
 
-# Définir la liste des chemins des dossiers contenant les photos
-#folder_paths = ['data/ange', 'data/clown','data/goldfish','data/lion','data/scalaire']
-new_path = 'new_data/'
+    # Pour chaque poisson dans la liste
+    for folder_path in poisson:
+        # Récupérer la liste de tous les fichiers jpg dans le dossier correspondant de "data"
+        file_list = os.listdir('data/'+folder_path)
 
-# Pour chaque dossier de la liste
-for folder_path in poisson:
+        # Pour chaque fichier dans la liste
+        for i, file_name in enumerate(file_list):
+            if(os.path.splitext(file_name)[1]=='.jpg'):
 
-    # Récupérer la liste de tous les fichiers dans le dossier
-    file_list = os.listdir('data/'+folder_path)
+        # Charger l'image en utilisant cv2.imread()
+                image = cv2.imread('data/'+folder_path+'/'+file_name)
+                if image is None:
+                    print("bug")
 
-    # Pour chaque fichier dans la liste
-    for i, file_name in enumerate(file_list):
-        if(os.path.splitext(file_name)[1]=='.jpg'):
+                # Redimensionnement de l'image en utilisant cv2.resize() à une taille de 100x100
+                new_image = cv2.resize(image, dsize=(100, 100), interpolation=cv2.INTER_CUBIC)
 
-    # Charger l'image en utilisant la librairie image de votre choix
-            image = cv2.imread('data/'+folder_path+'/'+file_name)
-            if image is None:
-                print("bug")
-        # Obtenir les dimensions de l'image
+                # vérifier si l'image existe déjà dans le nouveau dossier de sortie
+                if not os.path.exists(new_path + '/' + file_name):
+                    # Enregistrer l'image redimensionnée dans le nouveau dossier de sortie
+                    cv2.imwrite(new_path+folder_path+'/'+file_name, new_image)
 
+    # Initialiser le compteur de fichiers à 0
+    num_files = 0
 
-            new_image = cv2.resize(image, dsize=(100, 100), interpolation=cv2.INTER_CUBIC)
+    # Parcourir le répertoire "new_data" et compter les fichiers
+    for root, dirs, files in os.walk(new_path):
+        num_files += len(files)
 
-            if not os.path.exists(new_path + '/' + file_name):
-        # Enregistrer l'image redimensionnée
-                cv2.imwrite(new_path+folder_path+'/'+file_name, new_image)
-
-# Initialiser le compteur de fichiers à 0
-num_files = 0
-
-# Parcourir le répertoire et compter les fichiers
-for root, dirs, files in os.walk(new_path):
-    num_files += len(files)
-
-print(f'Le répertoire contient {num_files} fichiers.')
+    #afficher le nombre de fichiers final
+    print(f'Le répertoire contient {num_files} fichiers.')
